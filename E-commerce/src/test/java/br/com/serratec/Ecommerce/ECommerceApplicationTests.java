659351aba044125fc4bbe9782796b5d37ec38b1f
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-05-31 19:42:24 -0300

    Exceptions Básicas

---
+++ E-commerce/src/test/java/br/com/serratec/Ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
