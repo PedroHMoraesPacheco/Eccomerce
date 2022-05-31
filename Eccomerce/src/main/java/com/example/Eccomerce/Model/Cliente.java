d60590f3d56efcb6be4d25bb343d8e4a0c588208
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-05-31 18:52:41 -0300

    UsuarioDeletado

--- Eccomerce/src/main/java/com/example/Eccomerce/Model/Cliente.java
+++ Eccomerce/src/main/java/com/example/Eccomerce/Model/Cliente.java
@@ -1,5 +1,20 @@
 package com.example.Eccomerce.Model;
 
-public class Cliente {
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Embedded
+	private Usuario getUsuario;
 }
