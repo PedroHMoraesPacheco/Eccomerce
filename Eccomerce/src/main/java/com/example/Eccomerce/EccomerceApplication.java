f001e6f0dfcf62e832b6c4b640fe0d75f273e1f8
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-05-31 15:45:51 -0300

    Add files via upload

---
+++ Eccomerce/src/main/java/com/example/Eccomerce/EccomerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceApplication.class, args);
+	}
+
+}
---
+++ Eccomerce/src/test/java/com/example/conta/EccomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
