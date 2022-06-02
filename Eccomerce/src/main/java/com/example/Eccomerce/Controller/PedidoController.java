866f09a689213692b5781f5cc7151af43dba4668
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-06-02 17:05:24 -0300

    Pedido Controller

---
+++ Eccomerce/src/main/java/com/example/Eccomerce/Controller/PedidoController.java
@@ -0,0 +1,49 @@
+package com.example.Eccomerce.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Eccomerce.Exception.PedidoExisteException;
+import com.example.Eccomerce.Exception.PedidoNaoEcontradoException;
+import com.example.Eccomerce.Model.Pedido;
+import com.example.Eccomerce.Service.PedidoService;
+
+@RestController
+@RequestMapping("/pedido")
+public class PedidoController {
+
+	@Autowired
+	PedidoService service;
+
+	@GetMapping("{numeroPedido}")
+	public Pedido getOne(@PathVariable Integer numeroPedido) throws PedidoNaoEcontradoException {
+		return service.listarPedido(numeroPedido);
+	}
+
+	@PutMapping("/{numeroPedido}")
+	public Pedido update(@RequestBody Pedido pedido, @PathVariable Integer numeroPedido)
+			throws PedidoNaoEcontradoException, PedidoExisteException {
+		return service.editarPedido(pedido, numeroPedido);
+	}
+
+	@DeleteMapping("/{numeroPedido}")
+	public void delete(@PathVariable Integer numeroPedido) throws PedidoNaoEcontradoException {
+		service.deletarPedido(numeroPedido);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> insert(@RequestBody Pedido pedido) throws PedidoExisteException {
+		service.criarPedido(pedido);
+
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+}
