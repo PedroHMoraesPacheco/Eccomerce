1e83a2318c056105bdcf735fa0d2f6e53022e5e3
João Marcos Bernardes <102327585+JM-Bernardes@users.noreply.github.com>
2022-06-03 20:42:03 -0300

    User

--- Eccomerce/src/main/java/com/example/Eccomerce/Service/UserService.java
+++ Eccomerce/src/main/java/com/example/Eccomerce/Service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.Eccomerce.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.Eccomerce.DTO.ClienteDTO;
+import com.example.Eccomerce.DTO.FuncionarioDTO;
+import com.example.Eccomerce.Model.Funcionario;
 import com.example.Eccomerce.Model.User;
 import com.example.Eccomerce.Repository.UserRepository;
 
@@ -22,5 +26,27 @@ public class UserService {
 		novoUser.setUsername(clienteDto.getUsername());
 		userRepo.save(novoUser);
 		return novoUser;
+	}
+	
+	public User TransformaUserFuncionarioDto(FuncionarioDTO funcionarioDto) {
+		User novoUser=new User();
+		novoUser.setEmail(funcionarioDto.getEmail());
+		novoUser.setRole(funcionarioDto.getRole());
+		novoUser.setSenha(funcionarioDto.getSenha());
+		novoUser.setUsername(funcionarioDto.getUsername());
+		userRepo.save(novoUser);
+		return novoUser;
+	}
+	
+	public List<User> retorneTODOS() {
+		return userRepo.findAll();
+	}
+	
+	public User userByID(Integer id) {
+		return userRepo.findById(id).get();
+	}
+	
+	public void Delete(Integer id) {
+		userRepo.deleteById(id);
 	}
 }
