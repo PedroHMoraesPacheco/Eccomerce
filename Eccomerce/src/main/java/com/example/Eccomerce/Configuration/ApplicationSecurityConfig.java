b2675afc24d0f87c6360561615e944a151c5b789
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-06-17 17:43:27 -0300

    aa

--- Eccomerce/src/main/java/com/example/Eccomerce/Configuration/ApplicationSecurityConfig.java
+++ Eccomerce/src/main/java/com/example/Eccomerce/Configuration/ApplicationSecurityConfig.java
@@ -1,17 +1,54 @@
 package com.example.Eccomerce.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.Eccomerce.Security.AuthService;
+import com.example.Eccomerce.Security.JWTAutheticationFilter;
+import com.example.Eccomerce.Security.JWTAuthorizationFilter;
+import com.example.Eccomerce.Security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
-			.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-		
+	@Autowired
+	AuthService service;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	JWTUtil jwtUtil;
+	
+	//private static String[] AUTH_WHITELIST = {
+			
+		//	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/login", "/cliente","/cliente/**").permitAll().anyRequest().authenticated();
+		http.addFilterBefore(new JWTAutheticationFilter(authenticationManager(), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service).passwordEncoder(bCryptPasswordEncoder);
+	}
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- Eccomerce/src/main/java/com/example/Eccomerce/Controller/ClienteController.java
+++ Eccomerce/src/main/java/com/example/Eccomerce/Controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.example.Eccomerce.Exception.ProdutoNotExcepetion;
 import com.example.Eccomerce.Model.Cliente;
 import com.example.Eccomerce.Model.Pedido;
 import com.example.Eccomerce.Service.ClienteService;
+import com.example.Eccomerce.Service.UserService;
 
 @RestController
 @RequestMapping("/cliente")
@@ -28,6 +30,10 @@ public class ClienteController {
 	@Autowired
 	ClienteService clienteService;
 
+	@Autowired
+	UserService userS;
+	
+	@PreAuthorize("hasRole('cliente')")  
 	@GetMapping
 	public List<ClienteDTO> getTodos() {
 		List<Cliente> list = clienteService.RetorneTodos();
